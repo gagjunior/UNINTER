26ed82bc2b8541f1d85d596864921ca51d5697d3
Gilberto <gilberto.junior@cbbasfaltos.com.br>
2021-09-27 13:50:46 -0300

    Metodo setModoJogo

--- JAVA/jogo-da-velha/src/br/com/gagjunior/jv/Jogo.java
+++ JAVA/jogo-da-velha/src/br/com/gagjunior/jv/Jogo.java
@@ -8,11 +8,12 @@ public class Jogo {
 	public static void main(String[] args) {
 		
 		String nomeJogador;
-		int modoJogo;
-		int opcaoSimbolo;
+		int modoJogo = 0;
+		int opcaoSimbolo = 0;
 		String simboloHum;
 		String simboloComp;
 		Tabuleiro tab = new Tabuleiro();
+		Computador comp = null;
 		
 		//Titulo do jogo
 		exibirTitulo();		
@@ -24,7 +25,7 @@ public class Jogo {
 		
 		System.out.println(nomeJogador + ", Seja bem vindo ao jogo da velha!!\n");
 		
-		System.out.println("Escolha com qual simbolo vocÃª quer jogar...");
+		System.out.println("Escolha com qual simbolo você quer jogar...");
 		System.out.println("1 - X");
 		System.out.println("2 - O");
 		System.out.print("Digite o numero do simbolo: \n");
@@ -35,16 +36,11 @@ public class Jogo {
 		simboloHum = opcaoSimbolo == 1 ? "X" : "O";
 		simboloComp = simboloHum.equals("X") ? "O" : "X";
 		
-		System.out.println("VocÃª vai jogar com o simbolo: " + simboloHum + "\n");
+		System.out.println("Você vai jogar com o simbolo: " + simboloHum + "\n");
 		
-		System.out.println("***Modos de Jogo***");
-		System.out.println("1 - Modo A");
-		System.out.println("2 - Modo B");
-		System.out.println("3 - Modo C");
-		System.out.print("Digite o numero do modo que deseja jogar: ");
 		
-		modoJogo = teclado.nextInt();
-		teclado.nextLine();		
+		
+		Computador maquina = setModoJogo(modoJogo, comp, simboloComp, teclado);
 		
 		
 		teclado.close();
@@ -57,6 +53,30 @@ public class Jogo {
 		System.out.println("X      JOGO DA VELHA        O");
 		System.out.println("XOXOXOXOXOXOXOXOXOXOXOXOXOXOX");
 		System.out.println();		
+	}
+	
+	// Setar o modo de jogo
+	static Computador setModoJogo (int modo, Computador comp, String simbolo, Scanner teclado) {
+		System.out.println("***Modos de Jogo***");
+		System.out.println("1 - Modo A");
+		System.out.println("2 - Modo B");
+		System.out.println("3 - Modo C");
+		System.out.print("Digite o numero do modo que deseja jogar: ");
+		
+		modo = teclado.nextInt();		
+		teclado.nextLine();
+		
+		if (modo == 1) {
+			comp = new ModoA(simbolo);
+			System.out.println("Você vai jogar no modo 'A'");
+		} else if (modo == 2) {
+			comp = new ModoB(simbolo);			
+			System.out.println("Você vai jogar no modo 'B'");
+		} else {
+			comp = new ModoC(simbolo);
+			System.out.println("Você vai jogar no modo 'C'");
+		}
+		return comp;
 	}
 
 }
